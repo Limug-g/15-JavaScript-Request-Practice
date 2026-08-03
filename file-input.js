829fff89a8d59bd26b1c4916559642d3a48630df
Limug-g <dmlrbs94@gmail.com>
2026-08-03 18:22:31 +0900

    feature: Add file size checking code in file-input.js file

--- file-input.js
+++ file-input.js
@@ -1,3 +1,6 @@
+// const BASE_URL = '';
+const MAX_SIZE = 5 * 1024 * 1024; // -> 파일 최대용량
+
 const uploadBtn = document.querySelector("#uploadBtn");
 const imageInput = document.querySelector("#imageInput");
 const titleInput = document.querySelector("#titleInput");
@@ -5,15 +8,28 @@ const titleInput = document.querySelector("#titleInput");
 uploadBtn.addEventListener("click", async function upLoad() {
   try {
     //1. 사용자가 선택한 파일 가져오기
-    const imageFiles = imageInput.files[0];
+    const file = imageInput.files[0];
     //.files 속성은 input type ="file" 이 갖고 있는 속성이다.
     //이는 input에 등록한 파일들을 '배열처럼' 갖고 있는다
 
     //2. FormData 객체 만들기
     //FormData 객체는 JS에서 제공하는 생성자이다.
     //FormData에는 .append라는 속성이 내장되어있다.
+
+    //1.5. 파일 용량 체크 (프론트엔드 사이드)
+    //file && file.size 이거 두개를 같이 조건문에 넣은 이유
+    //-> 방어 코드: 사용자가 파일을 선택하지 않고 취소 할 경우
+    //imageInput.file[0] = undefined로 나옴
+    //undefined에 size를 확인 할 수 없으므로 에러가 발생
+    //이를 막기위해 방어코드를 작성한 것!
+    if (file && file.size > MAX_SIZE) {
+      alert("파일 크기는 5MB 미만으로 올려주세요");
+      imageInput.value = "";
+      return;
+    }
+
     const formData = new FormData();
-    formData.append("image", imageFiles);
+    formData.append("image", file);
     formData.append("title", titleInput.value);
 
     //3. fetch로 전송 - 업로드 기능의 API 엔드포인트 주소 사용
